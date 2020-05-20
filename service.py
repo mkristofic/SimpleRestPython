19838ed0c5ac425cf213a98fb6fda137a4b06086
Miro Krištofić <miro.kristof@gmail.com>
2020-05-20 16:18:06 +0200

    Changed auth plugin

--- service.py
+++ service.py
@@ -7,8 +7,9 @@ mysqldb = mysql.connector.connect(
   host="localhost",
   user="northwind",
   passwd="northwind",
-  database="northwind"
-)
+  database="northwind",
+  auth_plugin="mysql_native_password")
+
 cursor = mysqldb.cursor()
 
 app = Flask(__name__)
